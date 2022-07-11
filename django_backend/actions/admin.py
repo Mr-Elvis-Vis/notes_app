c501c8bfde2adbf7eb643f32af24a2b68c1041c5
mr-elvis-vis <elvis2025@yandex.ru>
2022-07-11 12:34:45 +0200

    added models/admin in django_backend.actions

--- django_backend/actions/admin.py
+++ django_backend/actions/admin.py
@@ -1,3 +1,36 @@
+from actions.models import Favourites, Sharing
 from django.contrib import admin
 
-# Register your models here.
+
+class FavouritesAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'note',
+        'date_favorite',
+    )
+    list_editable = ('note',)
+    search_fields = (
+        'user__username',
+        'user__email',
+        'note__name'
+    )
+    empty_value_display = '>empty<'
+
+
+class SharingAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'recipient',
+        'sharing_date',
+    )
+    search_fields = (
+        'user__username',
+        'user__email',
+        'recipient__username',
+        'recipient__email',
+    )
+    empty_value_display = '>empty<'
+
+
+admin.site.register(Favourites, FavouritesAdmin)
+admin.site.register(Sharing, SharingAdmin)
--- django_backend/actions/models.py
+++ django_backend/actions/models.py
@@ -1,3 +1,86 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from notes.models import Note
 
-# Create your models here.
+User = get_user_model()
+
+
+class Sharing(models.Model):
+    """
+    Providing access to your notes to other users
+    (for example, family members).
+    """
+    recipient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipient',
+        verbose_name='recipient',
+        help_text='the user to whom the notes were shared'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='accessor',
+        verbose_name='accessor',
+        help_text='the user who gave access to his notes'
+    )
+    sharing_date = models.DateField(
+        auto_now_add=True,
+        verbose_name='sharing date',
+        help_text='sharing date'
+    )
+
+    class Meta:
+        verbose_name = 'sharing'
+        verbose_name_plural = 'sharing'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'recipient'),
+                name='sharing_unique'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('recipient')),
+                name='not_sharing_myself'
+            ),
+        )
+
+    def __str__(self):
+        return f'{self.user} shared notes to {self.recipient}'
+
+
+class Favourites(models.Model):
+    """
+    Favorites for the most important or frequently used notes.
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorite_note',
+        verbose_name='owner of note',
+        help_text='the author who added the note to favorites'
+    )
+    note = models.ForeignKey(
+        Note,
+        on_delete=models.CASCADE,
+        related_name='favorite_note',
+        verbose_name='favorite note',
+        help_text='note added to favorites'
+    )
+    date_favorite = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='date added in favourites',
+        help_text='date the note was added to favorites'
+    )
+
+    class Meta:
+        ordering = ('-date_favorite',)
+        verbose_name = 'favourites'
+        verbose_name_plural = 'favourites'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'note'), name='favorite_unique'
+            ),
+        )
+
+    def __str__(self):
+        return f'{self.note} in favorites at {self.user}'
