0af96a4d7a92fb58f73255a7aa5055463cee5182
mr-elvis-vis <elvis2025@yandex.ru>
2022-07-11 13:28:26 +0200

    added notes app

--- django_backend/notes/admin.py
+++ django_backend/notes/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from notes.models import Category, CategoryNote, Note
 
-# Register your models here.
+
+class CategoryNoteAdmin(admin.TabularInline):
+    model = CategoryNote
+
+
+class NoteAdmin(admin.ModelAdmin):
+    inlines = (CategoryNoteAdmin,)
+    list_display = (
+        'author',
+        'name',
+        'text',
+    )
+    search_fields = (
+        'name',
+        'author__username',
+        'author__email'
+    )
+    list_filter = ('category',)
+    empty_value_display = '>empty<'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'color',
+        'slug',
+
+    )
+    list_editable = ('color',)
+    search_fields = (
+        'name',
+        'slug',
+    )
+    list_filter = ('name',)
+    empty_value_display = '>empty<'
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Note, NoteAdmin)
--- django_backend/notes/models.py
+++ django_backend/notes/models.py
@@ -1,3 +1,129 @@
+from colorfield.fields import ColorField
+from django.contrib.auth import get_user_model
+from django.core.validators import (MaxValueValidator, MinValueValidator,
+                                    validate_slug)
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    """
+    Categories to facilitate the classification and search of recipes. One
+    note can belong to several categories.
+    """
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        verbose_name='category title',
+        help_text='category name'
+    )
+    color = ColorField(
+        default='#FF0000',
+        unique=True,
+        verbose_name='HEX color',
+        help_text='color designation of the category'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='unique slug',
+        validators=(validate_slug,),
+        help_text='abbreviated category designation'
+    )
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'category'
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    """
+    Text note. May contain an image.
+    """
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='notes',
+        verbose_name='author of note',
+        help_text='the author/owner of the note'
+    )
+    category = models.ManyToManyField(
+        Category,
+        through='CategoryNote',
+        related_name='notes',
+        verbose_name='category',
+        help_text='category linked to the recipe'
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name='title',
+        help_text='name of the recipe'
+    )
+    image = models.ImageField(
+        upload_to='recipes_image/',
+        blank=True,
+        verbose_name='image of finished food',
+        help_text='image to explain the note'
+    )
+    text = models.TextField(
+        verbose_name='description',
+        help_text='note text'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='date of publication',
+        help_text='date of publication of the note'
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'note'
+        verbose_name_plural = 'notes'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('name', 'author',),
+                name='note_unique'
+            ),
+        )
+
+    def __str__(self):
+        return f'Note: {self.name}'
+
+
+class CategoryNote(models.Model):
+    """
+    A class linking tags and a recipe.
+    """
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='note_category',
+        verbose_name='category',
+        help_text='category linked to the note'
+    )
+    note = models.ForeignKey(
+        Note,
+        on_delete=models.CASCADE,
+        related_name='note_category',
+        verbose_name='note',
+        help_text='note'
+    )
+
+    class Meta:
+        verbose_name = 'category of note'
+        verbose_name_plural = 'category of note'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('category', 'note',),
+                name='category_note_unique'
+            ),
+        )
+
+    def __str__(self):
+        return f'Tag {self.category} of recipe {self.note}.'
